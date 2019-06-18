6f7b034f0f1b3a2734cbe65bc725e5f10f0b283a
Dipesh Bhatevera <35795916+DEEPESH98@users.noreply.github.com>
2019-06-18 15:51:24 +0530

    Create numpy_2D_array.py

---
+++ numpy_2D_array.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a=np.array([[1,2,3],[4,5,6],[1,2,3]])
+b=np.array([[1,2,3],[4,5,6],[1,2,3]])
+
+#printing part
+print(a)
+print(b)
+
+#Airthematic operations
+print(a*2)
+print(a+2)
+print(a/2)
+print(a**5)
+print(a+b)
+
+#perticular value calling
+
+print(a[0])
+print(b[0])
+
+print(a[0][0])
