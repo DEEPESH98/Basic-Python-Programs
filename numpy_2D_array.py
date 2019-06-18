8e638c4520438fce2c86dac808cd8b11f8670b87
Dipesh Bhatevera <35795916+DEEPESH98@users.noreply.github.com>
2019-06-18 16:09:11 +0530

    Update numpy_2D_array.py

--- numpy_2D_array.py
+++ numpy_2D_array.py
@@ -3,6 +3,10 @@ import numpy as np
 a=np.array([[1,2,3],[4,5,6],[1,2,3]])
 b=np.array([[1,2,3],[4,5,6],[1,2,3]])
 
+#shape check ker ne ke liye
+print(a.shape)
+print(b.shape)
+
 #printing part
 print(a)
 print(b)
@@ -20,3 +24,22 @@ print(a[0])
 print(b[0])
 
 print(a[0][0])
+
+
+#other operations
+print(a[0:2])
+
+print("\n",a[0:])
+
+#perticular colum ke liye
+print("\n",a[0: ,0])
+
+print("\n",a[0: 1,0])
+
+print("\n",a[0: 2,0])
+
+print("\n",a[0:,0:2])
+      
+print("\n",a[0:,[0,2]])
+
+print("\n",a[[0,2]])
